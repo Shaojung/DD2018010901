3fb88cfc46426604b6efcb505b3b74a94d106aed
Teacher <shaojung@gmail.com>
2018-01-09 11:39:25 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/pcschool/dd2018010901/MainActivity.java
@@ -0,0 +1,32 @@
+package tw.com.pcschool.dd2018010901;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("ADD");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getTitle().equals("ADD"))
+        {
+            Toast.makeText(MainActivity.this, "使用者按下 ADD", Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
